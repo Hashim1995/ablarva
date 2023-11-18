07ed8a37bfb5ac078d789ee6528efda2e178d898
Hashim Hashimli <hashimli.hshm@gmail.com>
2023-11-18 17:36:00 +0400

    changes

--- src/modules/chat/components/index.tsx
+++ src/modules/chat/components/index.tsx
@@ -7,16 +7,16 @@ function Chat() {
   return (
     <div className=" container-fluid h-full mx-auto ">
       <div className="grid lg:grid-cols-12 lg:grid-rows-5 h-768 lg:gap-10 md:grid md:grid-cols-12 md:grid-rows-8 md:gap-10">
-        <div className="bg-green-500 rounded-3xl lg:row-span-5 md:row-span-8">
+        <div className=" rounded-3xl lg:row-span-5 md:row-span-8">
           <LeftBar />
         </div>
         <div className="bg-blue-500 rounded-3xl lg:col-span-8 lg:row-span-5 md:col-span-11 md:row-span-5">
           <Messenger />
         </div>
-        <div className=" bg-purple-500 rounded-3xl  lg:col-span-3  lg:row-span-3 lg:col-start-10 md:col-span-7 md:row-span-3 md:col-start-2 md:row-start-6">
+        <div className=" rounded-3xl  lg:col-span-3  lg:row-span-3 lg:col-start-10 md:col-span-7 md:row-span-3 md:col-start-2 md:row-start-6">
           <ChatHistory />
         </div>
-        <div className="bg-yellow-500 rounded-3xl lg:col-span-3 lg:row-span-2 lg:col-start-10 lg:row-start-4 md:col-span-4 md:row-span-3 md:col-start-9 md:row-start-6">
+        <div className=" rounded-3xl lg:col-span-3 lg:row-span-2 lg:col-start-10 lg:row-start-4 md:col-span-4 md:row-span-3 md:col-start-9 md:row-start-6">
           <MyPlan />
         </div>
       </div>
--- src/modules/chat/components/leftbar/leftbar.tsx
+++ src/modules/chat/components/leftbar/leftbar.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import { BsPlusLg } from 'react-icons/bs';
+import { Button } from '@nextui-org/react';
 
 function LeftBar() {
-  return <div>LeftBar</div>;
+  return (
+    <div className="flex items-center justify-center h-full">
+      <Button isIconOnly className="bg-black rounded-full" size="sm">
+        <BsPlusLg size={23} color="white" />
+      </Button>
+    </div>
+  );
 }
 
 export default LeftBar;
