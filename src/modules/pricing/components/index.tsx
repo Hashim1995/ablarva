3ec95e71dcc25b9c1f49ccee1b1dfca3d151c86b
Narmin Huseynova <90983090+NarminHuseynova@users.noreply.github.com>
2023-11-22 14:19:41 +0400

    changed

--- src/modules/pricing/components/index.tsx
+++ src/modules/pricing/components/index.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-no-useless-fragment */
-// import { Button, Card, CardBody, Tab, Tabs } from '@nextui-org/react';
 import { pricingList1, pricingList2, pricingList3 } from '@/assets/dummy';
 import { useState, useEffect } from 'react';
 import { Button, Spinner } from '@nextui-org/react';
 import { BsArrowRightShort, BsFillXCircleFill } from 'react-icons/bs';
+import { dictionary } from '@/utils/constants/dictionary';
 import { IPricingListBody, IPricingListHeader } from '../types';
 
 function Pricing() {
@@ -15,7 +16,7 @@ function Pricing() {
 
   const [loading, setLoading] = useState<boolean>(false);
 
-  useEffect(() => {
+  function changeTab() {
     setLoading(true);
     if (activetab === 1) {
       setData(pricingList1);
@@ -29,6 +30,10 @@ function Pricing() {
       setData(pricingList3);
       setLoading(false);
     }
+  }
+
+  useEffect(() => {
+    changeTab();
   }, [activetab]);
 
   return (
@@ -41,42 +46,42 @@ function Pricing() {
               activetab === 1
                 ? '!bg-black !text-white'
                 : '!bg-white !text-black'
-            } mb-3 text-[16px] md:text-[18px] xl:text-[20px] w-[180px] xl:w-[200px] h-[50px] mr-[10px] xl:mr-0 bg-white hover:!bg-black hover:!text-white border border-black font-bold`}
+            } mb-3 text-[16px] xl:text-[20px] w-[180px] xl:w-[200px] h-[42px] xl:h-[50px] mr-[10px] xl:mr-0 bg-white hover:!bg-black hover:!text-white border border-black font-bold`}
             type="submit"
             onClick={() => {
               setActiveTab(1);
             }}
           >
-            AI sadə
+            {dictionary.az.aiSimple}
           </Button>
           <Button
             className={`${
               activetab === 2
                 ? '!bg-black !text-white'
                 : '!bg-white !text-black'
-            } mb-3 text-[16px] md:text-[18px] xl:text-[20px] w-[180px] xl:w-[200px] h-[50px] mr-[10px] xl:mr-0 bg-white hover:!bg-black hover:!text-white border border-black font-bold`}
+            } mb-3 text-[16px] xl:text-[20px] w-[180px] xl:w-[200px] h-[42px] xl:h-[50px] mr-[10px] xl:mr-0 bg-white hover:!bg-black hover:!text-white border border-black font-bold`}
             type="submit"
             onClick={() => {
               setActiveTab(2);
             }}
           >
-            AI köməkçi ilə
+            {dictionary.az.aiAssistant}
           </Button>
           <Button
             className={`${
               activetab === 3
                 ? '!bg-black !text-white'
                 : '!bg-white !text-black'
-            } mb-3 text-[16px] md:text-[18px] xl:text-[20px] w-[180px] xl:w-[200px] h-[50px] bg-white hover:!bg-black hover:!text-white border border-black font-bold`}
+            } mb-3 text-[16px] xl:text-[20px] w-[180px] xl:w-[200px] h-[42px] xl:h-[50px] bg-white hover:!bg-black hover:!text-white border border-black font-bold`}
             type="submit"
             onClick={() => {
               setActiveTab(3);
             }}
           >
-            Hamısı birində
+            {dictionary.az.allInOne}
           </Button>
         </div>
-        <div className="col-span-12 xl:col-span-10 row-span-6 xl:col-start-3 bg-white p-3 rounded-2xl">
+        <div className="col-span-12 xl:col-span-10 row-span-6 xl:col-start-3 bg-white rounded-2xl">
           {!loading ? (
             <table className="transition ease-in-out delay-150 duration-300">
               <thead className="border-b-1">
@@ -85,38 +90,38 @@ function Pricing() {
                     {hItem.price ? (
                       <td
                         key={window.crypto.randomUUID()}
-                        className="lg:h-[262px] text-center w-max bg-transparent border-r-2 last:border-r-0"
+                        className="xl:h-[262px] text-center w-max bg-transparent border-r-2 last:border-r-0"
                       >
-                        <div className="flex flex-col items-center h-full px-2.5 py-5">
-                          <p className="text-[16px] lg:text-[18px] xl:text-[20px] font-bold leading-6">
+                        <div className="flex flex-col items-center h-full px-2.5 py-3 xl:py-5">
+                          <p className="text-[16px] xl:text-[20px] font-bold leading-6 h-[45px] sm:h-auto">
                             {hItem.title || 'test'}
                           </p>
-                          <div className="flex flex-row items-center py-3 lg:py-6">
-                            <p className="text-[24px] md:text-[30px] xl:text-[40px] font-bold leading-6">
+                          <div className="flex flex-row items-center py-2 xl:py-6">
+                            <p className="text-[20px] sm:text-[24px] md:text-[30px] xl:text-[40px] font-bold leading-6">
                               {hItem.price || 'test'} ₼
                             </p>
-                            <p className="text-[18] lg:text-[24px] leading-6 mb-[-3px] font-bold text-[#C3C1C1]">
-                              /ay
+                            <p className="text-[16px] sm:text-[18] xl:text-[24px] leading-6 mb-[-3px] font-bold text-[#C3C1C1]">
+                              /{dictionary.az.month}
                             </p>
                           </div>
-                          <p className="w-fit text-[#C3C1C1] text-[14px] lg:text-[16px] leading-5 lg:leading-6 lg:px-5 pb-3 lg:pb-6">
+                          <p className="w-fit text-[#C3C1C1] text-[14px] xl:text-[16px] leading-5 leading-4 xl:leading-6 xl:px-5 pb-2 xl:pb-6">
                             {hItem.desciption || 'test'}
                           </p>
                           <Button
-                            className="bg-black text-white text-[14px] lg:text-[16px] border py-5 px-7"
+                            className="bg-black text-white text-[14px] xl:text-[16px] border py-5 px-2 sm:px-5 xl:px-7"
                             type="submit"
                             endContent={<BsArrowRightShort size={20} />}
                           >
-                            İndi qoşul
+                            {dictionary.az.joinNow}
                           </Button>
                         </div>
                       </td>
                     ) : (
                       <td
                         key={window.crypto.randomUUID()}
-                        className="lg:h-[262px] w-[189px] bg-black text-white text-[16px] lg:text-[18px] xl:text-[20px] text-center rounded-tl-2xl"
+                        className="xl:h-[262px] w-[189px] bg-black text-white text-[16px] xl:text-[20px] px-2 text-center rounded-tl-2xl"
                       >
-                        Tarifə nələr daxildir?
+                        {dictionary.az.tariffTitle}
                       </td>
                     )}
                   </>
@@ -128,35 +133,35 @@ function Pricing() {
                     className="w-[189px] odd:bg-white even:bg-slate-100"
                     key={window.crypto.randomUUID()}
                   >
-                    <td className="text-center text-[16px] lg:text-[18px] xl:text-[20px] py-3 px-3 font-bold border-r-2 last:border-r-0">
+                    <td className="text-center text-[14px] sm:text-[16px] xl:text-[20px] py-3 px-3 font-bold border-r-2 last:border-r-0">
                       {rItem.title}
                     </td>
-                    <td className="text-center text-[16px] lg:text-[18px] xl:text-[20px] py-3 px-3 font-medium border-r-2 last:border-r-0">
+                    <td className="text-center text-[14px] sm:text-[16px] xl:text-[20px] py-3 px-3 font-medium border-r-2 last:border-r-0">
                       {rItem.chatLimit || (
                         <div className="w-auto bg-transparent flex items-center justify-center">
                           <BsFillXCircleFill
                             style={{ fill: '#EB0000' }}
-                            size={34}
+                            className="text-[20px] sm:text-[24px] xl:text-[34px]"
                           />
                         </div>
                       )}
                     </td>
-                    <td className="text-center text-[16px] lg:text-[18px] xl:text-[20px] py-3 px-3 font-medium border-r-2 last:border-r-0">
+                    <td className="text-center text-[14px] sm:text-[16px] xl:text-[20px] py-3 px-3 font-medium border-r-2 last:border-r-0">
                       {rItem.imgLimit || (
                         <div className="w-auto bg-transparent flex items-center justify-center">
                           <BsFillXCircleFill
                             style={{ fill: '#EB0000' }}
-                            size={34}
+                            className="text-[20px] sm:text-[24px] xl:text-[34px]"
                           />
                         </div>
                       )}
                     </td>
-                    <td className="text-center text-[16px] lg:text-[18px] xl:text-[20px] py-3 px-3 font-medium border-r-2 last:border-r-0">
+                    <td className="text-center text-[14px] sm:text-[16px] xl:text-[20px] py-3 px-3 font-medium border-r-2 last:border-r-0">
                       {rItem.voiceLimit || (
                         <div className="w-auto bg-transparent flex items-center justify-center">
                           <BsFillXCircleFill
                             style={{ fill: '#EB0000' }}
-                            size={34}
+                            className="text-[20px] sm:text-[24px] xl:text-[34px]"
                           />
                         </div>
                       )}
@@ -166,7 +171,7 @@ function Pricing() {
               </tbody>
             </table>
           ) : (
-            <Spinner />
+            <Spinner className="flex items-center justify-center h-full" />
           )}
         </div>
       </div>
