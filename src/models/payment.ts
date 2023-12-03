1ef039e1d5d9d8985486c754df90b6c73cb7274f
Hashim Hashimli <hashimli.hshm@gmail.com>
2023-12-03 23:43:47 +0400

    changes

---
+++ src/models/payment.ts
@@ -0,0 +1,20 @@
+export interface IPricingTableHeader {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+}
+
+export interface IPricingTableBody {
+    chatLimit: number;
+    imageLimit: number;
+    voiceLimit: number;
+    assistantLimit: number;
+}
+
+export interface IPricingData {
+    categoryId: number;
+    categoryName: string;
+    tableHeaders: IPricingTableHeader[];
+    tableBodies: IPricingTableBody[];
+}
---
+++ src/services/payment-services/payment-services.ts
@@ -0,0 +1,38 @@
+/* eslint-disable no-empty-function */
+/* eslint-disable no-useless-constructor */
+/* eslint-disable class-methods-use-this */
+
+import { IGlobalResponse } from '@/models/common';
+import { IPricingData } from '@/models/payment';
+import { ErrorCallBack, HttpUtil } from '../adapter-config/config';
+
+
+export interface IPricingDataResponse extends IGlobalResponse {
+    data: IPricingData
+}
+
+export class PaymentService {
+    // eslint-disable-next-line no-use-before-define
+    private static instance: PaymentService | null;
+
+    private constructor() { }
+
+    public static getInstance(): PaymentService {
+        if (!this.instance) {
+            PaymentService.instance = new PaymentService();
+        }
+        return PaymentService.instance!;
+    }
+
+    public async getPricingList(id: number, onError?: ErrorCallBack): Promise<IPricingDataResponse> {
+        const res = await HttpUtil.get(
+            `api/client/subscriptions/Packages/${id}`,
+            null,
+            false,
+            onError
+        );
+        return res;
+    }
+
+
+}
