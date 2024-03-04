4ac7fd24d06fa7b785db0a298d8c4e000d6c2961
Hashim Hashimli <hashimli.hshm@gmail.com>
2024-03-04 17:19:35 +0400

    changes

--- src/redux/core/core-slice.ts
+++ src/redux/core/core-slice.ts
@@ -2,18 +2,22 @@ import { LayoutLanguage } from '@/models/common';
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 const coreSlice = createSlice({
-    name: 'core',
-    initialState: {
-        currentLayoutLanguage: LayoutLanguage.Azerbaijani, // Corrected the property name here
-    },
-    reducers: {
-        setCurrentLayoutLanguage: (state, action: PayloadAction<LayoutLanguage>) => { // Corrected the method name
-            state.currentLayoutLanguage = action.payload;
-        },
+  name: 'core',
+  initialState: {
+    currentLayoutLanguage: LayoutLanguage.Azerbaijani // Corrected the property name here
+  },
+  reducers: {
+    setCurrentLayoutLanguage: (
+      state,
+      action: PayloadAction<LayoutLanguage>
+    ) => {
+      // Corrected the method name
+      state.currentLayoutLanguage = action.payload;
     }
+  }
 });
 
 export const {
-    setCurrentLayoutLanguage // Corrected the export
+  setCurrentLayoutLanguage // Corrected the export
 } = coreSlice.actions;
 export default coreSlice.reducer;
