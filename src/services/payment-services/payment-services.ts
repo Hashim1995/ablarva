58d20aaf39608b6925c9eacbeada53bbd5344e01
Hashim Hashimli <hashimli.hshm@gmail.com>
2024-04-26 10:10:06 +0400

    changes

--- src/services/payment-services/payment-services.ts
+++ src/services/payment-services/payment-services.ts
@@ -47,7 +47,7 @@ export class PaymentService {
    * Constructs a new instance of the PaymentService class.
    * Private to enforce the singleton pattern.
    */
-  private constructor() { }
+  private constructor() {}
 
   /**
    * Gets the singleton instance of the PaymentService class.
