877fcb7769f15e6076e3507e481ce7a897d1f3e3
Hashim Hashimli <hashimli.hshm@gmail.com>
2024-05-10 12:46:48 +0400

    pending mails

--- src/modules/EMA/pending-mails/types.ts
+++ src/modules/EMA/pending-mails/types.ts
@@ -1,12 +1,12 @@
 interface IPendingListItem {
-    to: string;
-    id: string | number;
-    campaignName: string;
-    language: string;
-    templateType: string;
-    title: string;
-    createdAt: any;
-    description: string;
+  to: string;
+  id: string | number;
+  campaignName: string;
+  language: string;
+  templateType: string;
+  title: string;
+  createdAt: any;
+  description: string;
 }
 
-export type { IPendingListItem }
+export type { IPendingListItem };
